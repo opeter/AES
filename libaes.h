adc50d2ebc1ea5d84328871c0fc3bfc1831e2c91
Oliver Peter <oliver@gfuzz.de>
2018-11-16 16:59:54 +0100

    Replaced Macro Definitions by extern variables, use tables dynamically or static using #if

--- libaes.h
+++ libaes.h
@@ -7,42 +7,69 @@
 
 /*
  * The original Rijndael Algorithm would have support for different
- * input block sizes (128, 192, 256) howerver the AES paper only uses
+ * input block sizes (128, 192, 256) however the AES paper only uses
  * a single fixed block size which is 128 bit
  */
-#define AES_BLOCK_SIZE 128
+static const uint16_t AES_BLOCK_SIZE = 128;
 
-#define AES_WORD_LENGTH 32
+static const uint8_t AES_WORD_LENGTH = 32;
 
-#define AES_KEY_SIZE 128
-// #define AES_KEY_SIZE 192
-// #define AES_KEY_SIZE 256
+static const uint16_t AES_KEY_SIZE = 128;
+// static const uint16_t AES_KEY_SIZE = 192;
+// static const uint16_t AES_KEY_SIZE = 256;
 
 /* Section 5. Algorithm Specification, FIPS 197 */
-#define AES_ROUNDS (AES_KEY_SIZE / AES_WORD_LENGTH) + (AES_BLOCK_SIZE / AES_WORD_LENGTH) + 2
+static const uint16_t AES_ROUNDS = (AES_KEY_SIZE / AES_WORD_LENGTH) + (AES_BLOCK_SIZE / AES_WORD_LENGTH) + 2;
 
 /* Each round needs a subkey, plus 1 for the additional key whitening */
-#define AES_SUBKEYS AES_ROUNDS + 1
+static const uint16_t AES_SUBKEYS = AES_ROUNDS + 1;
 
 /* Iteration steps for w0, w1, w... within key schedule */
-#define AES_SUBKEY_PARTS AES_KEY_SIZE / AES_WORD_LENGTH
+static const uint16_t AES_SUBKEY_PARTS = AES_KEY_SIZE / AES_WORD_LENGTH;
 
+/* TODO */
+static const uint16_t AES_NK = AES_BLOCK_SIZE / AES_WORD_LENGTH;
+
+/* TODO */
+static const uint16_t AES_WI_RUNS = AES_ROUNDS * 4;
+
+/*
+ * We can either use hardcoded substitution tables
+ * or calculate them during runtime
+ * (0 = hardcoded / 1 = runtime)
+ */
+#define AES_CALCULATE_LOOKUP_TABLES 1
+
+#if AES_CALCULATE_LOOKUP_TABLES == 1
 /*
  * The non-reducible AES Polynomial in GF(2^8)
  *      P(x) = x^8 + x^4 + x^3 + x + 1
  */
-#define AES_POLYNOMIAL 0b100011011
-/* 0x03 / x+1 is the smallest generator in AES */
-#define AES_GENERATOR 0x03
+static const uint16_t AES_POLYNOMIAL = 0b100011011;
 
-void rotate_word(uint32_t *aes_word);
-uint8_t lsr8(uint8_t k, uint8_t p);
+/* 0x03 / x+1 is the smallest generator in AES */
+static const uint8_t AES_GENERATOR = 0x03;
+
+/* G L O B A L S */
+extern uint8_t  INITIALIZED;
+extern uint8_t  AES_BYTE_SUB[16][16];
+extern uint8_t  AES_INV_BYTE_SUB[16][16];
+extern uint32_t AES_KEY_SCHEDULE[AES_SUBKEYS][AES_SUBKEY_PARTS];
+extern uint32_t AES_RCON[10];
 
 int8_t get_shifts(uint16_t *factor);
+uint8_t lsr8(uint8_t k, uint8_t p);
 uint8_t aes_polynomial_division(uint16_t *factor);
 uint8_t multiplicative_inverse(uint8_t input);
 uint16_t multiply_polynomial(uint8_t factor1, uint8_t factor2);
 uint16_t exp_polynomial(uint16_t factor, uint8_t exponent);
+
+void gen_rcon_table(void);
+void gen_bytesub(void);
+void gen_inv_bytesub(void);
+#endif
+
+void rotate_word(uint32_t *aes_word);
 
 void key_addition();
 void byte_substitution();
@@ -52,13 +79,11 @@ uint8_t key_schedule(const uint16_t *key);
 
 uint8_t sub_byte(uint8_t b);
 uint8_t inv_sub_byte(uint8_t b);
+uint32_t rcon(uint8_t i);
 
 void inv_byte_substitution();
 void inv_shift_rows();
 void inv_mix_column();
-
-void gen_bytesub(void);
-void gen_inv_bytesub(void);
 
 uint8_t aes_init(const uint16_t *key);
 uint8_t aes_encrypt(const uint16_t *key, const uint16_t *input, uint16_t *output);
