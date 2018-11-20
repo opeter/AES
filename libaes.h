202cf1b4ccffbd35141cbc027735bcbd1001e1e3
Oliver Peter <oliver@gfuzz.de>
2018-11-20 18:02:55 +0100

    AES Decryption works

--- libaes.h
+++ libaes.h
@@ -3,43 +3,6 @@
 
 #include "config.h"
 
-#include <stdint.h>
-
-/*
- * The original Rijndael Algorithm would have support for different
- * input block sizes (128, 192, 256) however the AES paper only uses
- * a single fixed block size which is 128 bit
- */
-static const uint16_t AES_BLOCK_SIZE = 128;
-
-static const uint8_t AES_WORD_LENGTH = 32;
-
-static const uint16_t AES_KEY_SIZE = 128;
-// static const uint16_t AES_KEY_SIZE = 192;
-// static const uint16_t AES_KEY_SIZE = 256;
-
-/* Section 5. Algorithm Specification, FIPS 197 */
-static const uint16_t AES_ROUNDS = (AES_KEY_SIZE / AES_WORD_LENGTH) + (AES_BLOCK_SIZE / AES_WORD_LENGTH) + 2;
-
-/* Each round needs a subkey, plus 1 for the additional key whitening */
-static const uint16_t AES_SUBKEYS = AES_ROUNDS + 1;
-
-/* Iteration steps for w0, w1, w... within key schedule */
-static const uint16_t AES_SUBKEY_PARTS = AES_KEY_SIZE / AES_WORD_LENGTH;
-
-/* TODO */
-static const uint16_t AES_NK = AES_BLOCK_SIZE / AES_WORD_LENGTH;
-
-/* TODO */
-static const uint16_t AES_WI_RUNS = AES_ROUNDS * 4;
-
-/*
- * We can either use hardcoded substitution tables
- * or calculate them during runtime
- * (0 = hardcoded / 1 = runtime)
- */
-#define AES_CALCULATE_LOOKUP_TABLES 1
-
 #if AES_CALCULATE_LOOKUP_TABLES == 1
 /*
  * The non-reducible AES Polynomial in GF(2^8)
@@ -49,13 +12,6 @@ static const uint16_t AES_POLYNOMIAL = 0b100011011;
 
 /* 0x03 / x+1 is the smallest generator in AES */
 static const uint8_t AES_GENERATOR = 0x03;
-
-/* G L O B A L S */
-extern uint8_t  INITIALIZED;
-extern uint8_t  AES_BYTE_SUB[16][16];
-extern uint8_t  AES_INV_BYTE_SUB[16][16];
-extern uint32_t AES_KEY_SCHEDULE[AES_SUBKEYS][AES_SUBKEY_PARTS];
-extern uint32_t AES_RCON[10];
 
 int8_t get_shifts(uint16_t *factor);
 uint8_t lsr8(uint8_t k, uint8_t p);
@@ -70,7 +26,19 @@ void gen_inv_bytesub(void);
 void gen_multiplication(void);
 #endif
 
-void rotate_word(uint32_t *aes_word);
+/*
+ * AES Macro Definitions to handle lookup tables
+ */
+#define AES_MULTIPLY_0x2(x) (MULTIPLY_0x2[(x)])
+#define AES_MULTIPLY_0x3(x) (MULTIPLY_0x3[(x)])
+#define AES_MULTIPLY_0xE(x) (MULTIPLY_0xE[(x)])
+#define AES_MULTIPLY_0xB(x) (MULTIPLY_0xB[(x)])
+#define AES_MULTIPLY_0xD(x) (MULTIPLY_0xD[(x)])
+#define AES_MULTIPLY_0x9(x) (MULTIPLY_0x9[(x)])
+
+#define AES_RC(x) (RC[(x)])
+#define SUB_BYTE(x) AES_BYTE_SUB[((x) >> 4)][((x) & 0xF)]
+#define INV_SUB_BYTE(x) AES_INV_BYTE_SUB[((x) >> 4)][((x) & 0xF)]
 
 int8_t key_addition(uint32_t *key, uint8_t *state);
 int8_t byte_substitution(uint8_t *state);
@@ -78,12 +46,10 @@ int8_t shift_rows(uint8_t *state);
 int8_t mix_column(uint8_t *state);
 int8_t key_schedule(const uint8_t *key);
 
-uint8_t sub_byte(uint8_t b);
-uint8_t inv_sub_byte(uint8_t b);
-
-void inv_byte_substitution();
-void inv_shift_rows();
-void inv_mix_column();
+void rotate_word(uint32_t *aes_word);
+int8_t inv_byte_substitution(uint8_t *state);
+int8_t inv_shift_rows(uint8_t *state);
+int8_t inv_mix_column(uint8_t *state);
 
 uint8_t aes_init(const uint8_t *key);
 uint8_t aes_encrypt(const uint8_t *key, uint8_t *state);
