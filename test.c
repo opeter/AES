cb29ba5cc658d6b47652a5f52fac0c5ffe696115
Oliver Peter <oliver@gfuzz.de>
2018-11-20 18:02:40 +0100

    Implemented correct tests.

--- test.c
+++ test.c
@@ -11,7 +11,7 @@ main(void)
 {
     puts("*** BEGIN AES TESTS");
 
-    /* INIT */
+    /* Init AES Algorithm */
     aes_init(TEST_KEY);
 
     /* Encryption */
@@ -19,40 +19,39 @@ main(void)
     {
         printf(" * AES Encryption Round %2d\n", i);
         uint8_t state[16] = { 0 };
-        memcpy(state, TEST_INPUT, 16U);
+        memcpy(state, TEST_INPUT[i], 16U);
         aes_encrypt(TEST_KEY, state);
         for(int j = 0; j < 16; j++)
         {
-            if(state[j+(j*4)] != TEST_OUTPUT[i][j+(j*4)])
+            if(state[j] != TEST_OUTPUT[i][j])
             {
                 printf("  x Round %d, Block %2d do not match: 0x%02x != 0x%02x\n",
-                    i, j, state[j+(j*4)], TEST_OUTPUT[i][+(j*4)]);
+                    i, j, state[j], TEST_OUTPUT[i][j]);
+                    exit(1);
             }
-            else
-                puts("  * OK");
         }
-        puts("");
+        printf("   `--> Round %d successfully encrypted \n", i);
     }
     puts("-------------------------------------------------------------");
-    exit(0);
-    /* Decryption */
+
+    /* Encryption */
     for(int i = 0; i < ROUNDS; i++)
     {
         printf(" * AES Decryption Round %2d\n", i);
         uint8_t state[16] = { 0 };
-        memcpy(state, TEST_OUTPUT, 16U);
+        memcpy(state, TEST_OUTPUT[i], 16U);
         aes_decrypt(TEST_KEY, state);
         for(int j = 0; j < 16; j++)
         {
             if(state[j] != TEST_INPUT[i][j])
             {
                 printf("  x Round %d, Block %2d do not match: 0x%02x != 0x%02x\n",
-                    i, j, state[j], TEST_OUTPUT[i][j]);
+                    i, j, state[j], TEST_INPUT[i][j]);
+                exit(1);
             }
-            else
-                puts("  * OK");
         }
+        printf("   `--> Round %d successfully decrypted \n", i);
     }
-    puts("*** END OF PROGRAM");
+    puts("-------------------------------------------------------------");    puts("*** END OF PROGRAM");
     return 0;
 }
